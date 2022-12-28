6dea9c20a0bbf9002354aa3ef4fd197e450ed297
vuga <matvey20081010@mail.ru>
2022-12-28 17:32:04 +1000

    hello

---
+++ main.py
@@ -0,0 +1,22 @@
+# a = 10
+# b = 32
+# str_1 = "vuga"
+# str_2 = "hello"
+#
+# def main(a, b):
+#     return a + "," + b
+#
+# print(main(str_1, str_2))
+
+mass = []
+
+for i in range(10):
+ mass.append(i)
+print(len(mass))
+for i in range(len(mass)):
+ mass[i] = mass[i]*2
+
+
+
+
+print(mass)
